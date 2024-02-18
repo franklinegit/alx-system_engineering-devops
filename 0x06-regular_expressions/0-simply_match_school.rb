03bf01b47e6afd2d605e478bf38cf098e011919e
franklinegit <oddfrankline@gmail.com>
2024-02-18 22:41:44 +0000

    Regular expressions tasks from 0-7

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+
+# Check if the argument matches the regular expression
+puts ARGV[0].scan(/School/).join
---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+
+# Check if the argument matches the regular expression
+puts ARGV[0].scan(/[A-Z]/).join
